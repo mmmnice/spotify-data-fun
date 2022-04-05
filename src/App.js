3e37e50706203746481885b0235805c0f7160744
ray <raylimb15@gmail.com>
2022-04-05 08:39:28 -0400

    gitignore config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import axios from 'axios';
 
 //REFRESH BREAKS THE TOKEN FROM LOCAL STORAGE
 
 function App() {
+
  
 
   const [token, settoken] = useState('');
@@ -15,7 +16,7 @@ function App() {
   useEffect(() => {
     let hash = window.location.hash;
     let token = window.localStorage.getItem('token');
-    if(!token){
+    if (!token) {
       let param = new URLSearchParams(hash);
       token = param.get('#access_token');
       window.location.hash = '';
@@ -34,9 +35,9 @@ function App() {
   let searchArtists = async (e) => {
     e.preventDefault();
     console.log(token);
-    const {data} = await axios.get('https://api.spotify.com/v1/search' , {
-      headers:{
-        Authorization : `Bearer ${token}`
+    const { data } = await axios.get('https://api.spotify.com/v1/search', {
+      headers: {
+        Authorization: `Bearer ${token}`
       },
       params: {
         q: searchKey,
@@ -49,52 +50,52 @@ function App() {
 
   let searchPlaylists = async (e) => {
     console.log('error');
-    const {data} = await axios.get('https://api.spotify.com/v1/me/playlists' , {
-      headers:{
-        Authorization : `Bearer ${token}`
+    const { data } = await axios.get('https://api.spotify.com/v1/me/playlists', {
+      headers: {
+        Authorization: `Bearer ${token}`
       }
     })
     console.log(data);
 
-    
+
   }
 
   const renderArtists = () => {
     console.log('try', token);
-    if(!token){
+    if (!token) {
       console.log('im ')
       return null;
     } else {
       return artists.map(artist => (
         <div key={artist.id}>
-            {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
-            {artist.name}
+          {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt="" /> : <div>No Image</div>}
+          {artist.name}
         </div>
-    ))
+      ))
     }
-  
+
   }
   return (
     <div className="App">
       <header className="App-header">
         <h1>Spotify Reaact</h1>
-        {!token ? <a href = {`${Auth_Endpoint}?client_id=${Client_ID}&redirect_uri=${Redirect_URI}&response_type=${Response_type}`}>
-           Login in this bitch</a> :
-        <button onClick = {logout}>logout</button>
+        {!token ? <a href={`${Auth_Endpoint}?client_id=${Client_ID}&redirect_uri=${Redirect_URI}&response_type=${Response_type}`}>
+          Login in this bitch</a> :
+          <button onClick={logout}>logout</button>
 
         }
         {/* can separate this part to another component */}
         {renderArtists()}
-        <button onClick = {searchPlaylists}> Playlists</button>
-        {!token ? <h1>log in</h1> : 
-        <form onSubmit = {searchArtists}>
-          <input type = 'text' onChange = {e => setSearchKey(e.target.value)}>
-           
-          </input>
-          <button type ={'submit'}> 
+        <button onClick={searchPlaylists}> Playlists</button>
+        {!token ? <h1>log in</h1> :
+          <form onSubmit={searchArtists}>
+            <input type='text' onChange={e => setSearchKey(e.target.value)}>
+
+            </input>
+            <button type={'submit'}>
               Search
             </button>
-        </form>
+          </form>
         }
       </header>
     </div>
