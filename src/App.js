ea5b3d360400e92247867b2331b605d9b99730aa
ray <raylimb15@gmail.com>
2022-04-14 18:53:09 -0400

    Need to make promise for genres

--- src/App.js
+++ src/App.js
@@ -14,9 +14,8 @@ function App() {
   const Response_type = config.response_type;
 
   const [token, settoken] = useState('');
-  const [searchKey, setSearchKey] = useState('');
-  const [artists, setArtists] = useState([]);
   const [playlists, setPlaylists] = useState([]);
+  const [info, setGenres] = useState([]);
 
   //id
   const [selected, setSelected] = useState('');
@@ -44,46 +43,19 @@ function App() {
     window.localStorage.removeItem('token');
   }
 
-  let searchArtists = async (e) => {
-    e.preventDefault();
-    const { data } = await axios.get('https://api.spotify.com/v1/search', {
-      headers: {
-        Authorization: `Bearer ${token}`
-      },
-      params: {
-        q: searchKey,
-        type: 'artist'
-      }
-    })
-    setArtists(data.artists.items);
-    console.log(data);
-  }
-
   let searchPlaylists = async (e) => {
     console.log('error');
     const { data } = await axios.get('https://api.spotify.com/v1/me/playlists', {
       headers: {
-        Authorization: `Bearer ${token}`
+        Authorization: `Bearer ${token}`,
       }
     })
+    console.log(data);
     setPlaylists(data.items);
 
 
   }
 
-  const renderArtists = () => {
-    if (!token) {
-      return null;
-    } else {
-      return artists.map(artist => (
-        <div key={artist.id}>
-          {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt="" /> : <div>No Image</div>}
-          {artist.name}
-        </div>
-      ))
-    }
-
-  }
   const displayInfo = async (e) => {
     // console.log('hey', e);
     hey = {};
@@ -95,26 +67,10 @@ function App() {
 
     //mapping for the genre
     data.tracks.items.forEach(track => {
-      console.log(track.track, 'the track');
       get_genre(track.track);
     })
-    // get_artists(data);
 
   }
-
-
-//Multiple artists can be present in each song
-  // const get_artists = async (songs) => {
-  //   const { data } = await axios.get(`https://api.spotify.com/v1/tracks/${songs.tracks.items[0].track.id}`, {
-  //     headers: {
-  //       Authorization: `Bearer ${token}`,
-  //     }
-  //   })
-  //   console.log(data, 'tracks of the playlist')
-
-  //   get_genre(data);
-  // }
-
 
   const get_genre = async (song) => {
     // join the artist ids
@@ -131,22 +87,34 @@ function App() {
       }
     })
     data.artists.forEach(artist => {
-      console.log(artist.genres);
+      // console.log(artist.genres);
       artist.genres.forEach(genre => {
         //add genre
         hey[genre] = (hey[genre] + 1) || 1
-        
+
       })
     })
-    console.log(hey);
+
+    setGenres(hey);
   }
+
+
   const renderPlaylists = () => {
-    console.log(playlists);
     return playlists.map(playlist => (
-      <div key = {playlist.name} onClick ={() => displayInfo(playlist)}>
+      <div key={playlist.name} onClick={() => displayInfo(playlist)}>
         hey {playlist.name}
       </div>
     ))
+  }
+  const renderGenres = () => {
+    console.log(info, 'heyhey');
+    // setTimeout(() => {
+    //   for(var key in hey){
+    //     console.log(key);
+    //   }
+    // }, 2000);
+    console.log(hey);
+    
   }
   return (
     <div className="App">
@@ -158,18 +126,19 @@ function App() {
 
         }
         {/* can separate this part to another component */}
-        {renderArtists()}
-        {renderPlaylists()}
+        <div className='content'>
+          <div className='Playlists-nav'>
+            {renderPlaylists()}
+          </div>
+          <div className='actual-content'>
+            {renderGenres()}
+
+          </div>
+        </div>
+        
         <button onClick={searchPlaylists}> Playlists</button>
         {!token ? <h1>log in</h1> :
-          <form onSubmit={searchArtists}>
-            <input type='text' onChange={e => setSearchKey(e.target.value)}>
-
-            </input>
-            <button type={'submit'}>
-              Search
-            </button>
-          </form>
+          null
         }
       </header>
     </div>
